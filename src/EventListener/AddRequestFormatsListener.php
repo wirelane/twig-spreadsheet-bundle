73b59ac713bb50985a56a516956cf7d0838a2257
Bastian Konetzny <5451122+bkonetzny@users.noreply.github.com>
2022-06-02 14:47:30 +0200

    Updated to use RequestEvent

--- src/EventListener/AddRequestFormatsListener.php
+++ src/EventListener/AddRequestFormatsListener.php
@@ -3,18 +3,12 @@
 namespace MewesK\TwigSpreadsheetBundle\EventListener;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-/**
- * Class AddRequestFormatsListener.
- */
 class AddRequestFormatsListener implements EventSubscriberInterface
 {
-    /**
-     * @param GetResponseEvent $event
-     */
-    public function onKernelRequest(GetResponseEvent $event)
+    public function onKernelRequest(RequestEvent $event)
     {
         $event->getRequest()->setFormat('csv', 'text/csv');
         $event->getRequest()->setFormat('ods', 'application/vnd.oasis.opendocument.spreadsheet');
@@ -23,9 +17,6 @@ class AddRequestFormatsListener implements EventSubscriberInterface
         $event->getRequest()->setFormat('xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public static function getSubscribedEvents()
     {
         return [
